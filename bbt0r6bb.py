1940ebf1c3d613db4a763ca6d56444a90081c85a
ttand <wqazsedc@gmail.com>
2023-02-15 16:59:58 +0800

    config in bb nb

--- bbt0r6bb.py
+++ bbt0r6bb.py
@@ -65,6 +65,64 @@ def loadTR(TrFileName):
 
     return rawDataset.map(parseExample).batch(128)
 
+class BoxLoss(tf.keras.losses.Loss):
+  """L2 box regression loss."""
+
+  def __init__(self, delta=0.1, **kwargs):
+    """Initialize box loss.
+
+    Args:
+      delta: `float`, the point where the huber loss function changes from a
+        quadratic to linear. It is typically around the mean value of regression
+        target. For instances, the regression targets of 512x512 input with 6
+        anchors on P3-P7 pyramid is about [0.1, 0.1, 0.2, 0.2].
+      **kwargs: other params.
+    """
+    super().__init__(**kwargs)
+    self.huber = tf.keras.losses.Huber(
+        delta, reduction=tf.keras.losses.Reduction.NONE)
+
+  @tf.autograph.experimental.do_not_convert
+  def call(self, y_true, box_outputs):
+    num_positives, box_targets = y_true
+    normalizer = num_positives * 4.0
+    mask = tf.cast(box_targets != 0.0, box_outputs.dtype)
+    box_targets = tf.expand_dims(box_targets, axis=-1)
+    box_outputs = tf.expand_dims(box_outputs, axis=-1)
+    # TODO(fsx950223): remove cast when huber loss dtype is fixed.
+    box_loss = tf.cast(self.huber(box_targets, box_outputs),
+                       box_outputs.dtype) * mask
+    box_loss = tf.reduce_sum(box_loss) / normalizer
+    return box_loss
+
+
+class BoxIouLoss(tf.keras.losses.Loss):
+  """Box iou loss."""
+
+  def __init__(self, iou_loss_type, min_level, max_level, num_scales,
+               aspect_ratios, anchor_scale, image_size, **kwargs):
+    super().__init__(**kwargs)
+    self.iou_loss_type = iou_loss_type
+    self.input_anchors = anchors.Anchors(min_level, max_level, num_scales,
+                                         aspect_ratios, anchor_scale,
+                                         image_size)
+
+  @tf.autograph.experimental.do_not_convert
+  def call(self, y_true, box_outputs):
+    anchor_boxes = tf.tile(
+        self.input_anchors.boxes,
+        [box_outputs.shape[0] // self.input_anchors.boxes.shape[0], 1])
+    num_positives, box_targets = y_true
+    normalizer = num_positives * 4.0
+    mask = tf.cast(box_targets != 0.0, box_outputs.dtype)
+    box_outputs = anchors.decode_box_outputs(box_outputs, anchor_boxes) * mask
+    box_targets = anchors.decode_box_outputs(box_targets, anchor_boxes) * mask
+    box_iou_loss = iou_utils.iou_loss(box_outputs, box_targets,
+                                      self.iou_loss_type)
+    box_iou_loss = tf.reduce_sum(box_iou_loss) / normalizer
+    return box_iou_loss
+
+
 
 trainDataSet = loadTR('ssd0.tfr')
 testDataSet = loadTR('ssdt1.tfr')
@@ -124,4 +182,4 @@ converter = tf.lite.TFLiteConverter.from_keras_model(model)
 
 tflite_model_quant = converter.convert()
 
-open("modelq0.tflite", "wb").write(tflite_model_quant)
+open("modelqbb0.tflite", "wb").write(tflite_model_quant)
